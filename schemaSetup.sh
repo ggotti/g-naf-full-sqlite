af33e25ccd23859ece60c93460f018665fb1bb59
Gerard Gigliotti <ggigliotti@ippon.tech>
2021-04-21 22:21:27 +1000

    Fixed schema resolution issue

--- schemaSetup.sh
+++ schemaSetup.sh
@@ -3,6 +3,6 @@
 mkdir -p "/map"
 DATABASE_FILE="/map/fullGNAF.db"
 
-cat ddl.sql | sqlite3 ${DATABASE_FILE}
-cat addressView.sql | sqlite3 ${DATABASE_FILE}
+cat /root/scripts/ddl.sql | sqlite3 ${DATABASE_FILE}
+cat /root/scripts/addressView.sql | sqlite3 ${DATABASE_FILE}
 
